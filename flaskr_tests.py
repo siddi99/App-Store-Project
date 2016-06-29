5339bb26e363f248f9469365bcd749ff125b315e
Ayush Sharma <ayushsharma@Ayushs-MacBook-Pro.local>
2016-06-29 14:06:33 -0700

    Starting Point

---
+++ flaskr_tests.py
@@ -0,0 +1,23 @@
+import os
+import flaskr
+import unittest
+import tempfile
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        self.app = flaskr.app.test_client()
+        with flaskr.app.app_context():
+            flaskr.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert b'[]' in rv.data
+
+if __name__ == '__main__':
+    unittest.main()
